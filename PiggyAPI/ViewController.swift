e698f0e00b1d6ecebc6aadaef9baf0d1801aba4b
Agustin Malo <amalo@Agustins-MacBook-Pro.local>
2017-01-25 00:29:05 -0500

    Alamofire installed and linked to Djano REST

--- PiggyAPI/ViewController.swift
+++ PiggyAPI/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        newPost()
+//        getPosts()
+//        getPost()
+//        getUsers()
+        newUser()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,89 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func newPost(){
+        let postsEndPoint = "http://localhost:8000/api/posts/?format=json"
+        
+        let newPost = ["title": "AGUSTIN",
+                       "creator": "http://localhost:8000/api/customUsers/3/",
+                       "description": "AGUSTIN",
+                       "postType": "DR",
+                       "origin": "AGUSTIN",
+                       "destination": "AGUSTIN",
+                       "emptySeats": 1,
+                       "passengerCapacity": 2,
+                       "status": "A"] as [String : Any]
+        
+        
+        Alamofire.request(postsEndPoint, method: .post, parameters: newPost, encoding: JSONEncoding.default, headers: nil)
+            .responseJSON{
+                response in
+                debugPrint(response)
+        }
+    }
+    
+    func getPosts(){
+        let postsEndPoint = "http://localhost:8000/api/posts/?format=json"
+        
+        
+        Alamofire.request(postsEndPoint, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+            .responseJSON{
+                response in
+                debugPrint(response)
+        }
+    }
+    
+    
+    func getPost(){
+//        TODO: request specific post with parameters
+        let postsEndPoint = "http://localhost:8000/api/posts/?format=json"
+        
+        Alamofire.request(postsEndPoint, method: .get, parameters: ["id": "1"], encoding: JSONEncoding.default, headers: nil)
+            .responseJSON{
+                response in
+                debugPrint(response)
+        }
+    }
+    
+    
+    
+    func getUsers(){
+        let usersEndPoint = "http://localhost:8000/api/customUsers/?format=json"
+        
+        
+        Alamofire.request(usersEndPoint, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+            .responseJSON{
+                response in
+                debugPrint(response)
+        }
+    }
+    
+    func newUser(){
+        let usersEndPoint = "http://localhost:8000/api/customUsers/?format=json"
+        let user = ["first_name": "Charlie",
+                    "last_name": "Strong",
+                    "email": "strong@ufl.edu",
+                    "username": "alamo",
+                    "password": "firefirefire"]
+        
+        let newCustomUser = ["user": user,
+                             "rating": "3.11",
+                             "school": "alamo",
+                             "car": "fire",
+                             "phoneNumber": "19542979"] as [String : Any]
+        
+        
+        Alamofire.request(usersEndPoint, method: .post, parameters: newCustomUser, encoding: JSONEncoding.default, headers: nil)
+            .responseJSON{
+                response in
+                debugPrint(response)
+        }
+    }
+
+    
+    
+    
 
 }
 
